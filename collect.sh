bc86bf2376d45d021f5166f7881062bd69f79292
Holger Teichert <post@complanar.de>
2022-04-27 21:34:15 +0200

    Complete refactoring. Uncomplete revision.

--- collect.sh
+++ collect.sh
@@ -1,42 +1,124 @@
 #!/bin/bash
 
+#######################
+###  CONFIGURATION  ###
+#######################
+
+PORT="32400"
+IP_TEMPLATE="192.168.2.2xx"
+
+IP_PREFIX=${IP_TEMPLATE%%x*}
+PREFLEN=$(( ${#IP_TEMPLATE} - ${#IP_PREFIX} ))
+
+TIMEOUT=5
+
+REMOTE_USER="schueler"
+REMOTE_DIR="/home/${REMOTE_USER}/Schreibtisch/Austausch"
+
+LOCAL_USER="lehrer"
+LOCAL_DIR="/home/${LOCAL_USER}/Schreibtisch/Eingesammelt"
+
+FIRST_IP=1
+LAST_IP=15
+
+###################
+###  FUNCTIONS  ###
+###################
+
+# $1 device number: integer
+fetch_from() {
+  IP=$(generate_ip $i)
+  DEVICE=$(generate_host $i)
+
+  echo "Hole Daten von Gerät ${DEVICE} (IP: ${IP}) …"
+  scp -r -P $PORT ${REMOTE_USER}@${IP}:${REMOTE_DIR}/* ${LOCAL_DIR}/$DEVICE #&
+  #pid=$!
+
+  #sleep $TIMEOUT
+  #kill $pid
+  echo "    Von ${DEVICE} übertragen."
+}
+
+# $1 device number: integer
+send_to() {
+  IP=$(generate_ip $i)
+  DEVICE=$(generate_host $i)
+
+  echo "Sende Daten an Gerät ${DEVICE} (IP: ${IP}) …"
+  scp -r -P $PORT ${LOCAL_DIR}/${DEVICE}/* ${REMOTE_USER}@${IP}:${REMOTE_DIR} && -rm -r ${LOCAL_DIR}/${DEVICE}/* #&
+  #pid=$!
+
+  #sleep $TIMEOUT
+  #kill $pid
+  echo "    An ${DEVICE} übertragen."
+}
+
+# $1 number: integer
+# $1 desired number of digits: integer
+number_digits() {
+  number=$1
+  while [ ${#number} -lt $2 ]; do
+    number="0${number}"
+  done
+  echo $number
+}
+
+# $1 device number: integer
+generate_ip() {
+  echo "${IP_TEMPLATE%%x*}$(number_digits $1 $PREFLEN)"
+}
+
+# $1 device number: integer
+generate_host() {
+  echo "S$(number_digits $1 $PREFLEN)"
+}
+
+######################
+###  MAIN PROGRAM  ###
+######################
+
 zenity --question --text="Wollen Sie die Daten aller Rechner einsammeln?\n\nDieser Vorgang kann eine Weile dauern. Sie werden benachrichtigt, wenn das Einsammeln abgeschlossen ist." --width=250
 ANSWER="$?"
 
-if [ $ANSWER = "0" ]; then 
-    i=1
-    while [ $i -le 15 ]
-    do
-      if [ $i -le 9 ]; then
-        d="0${i}"
-      else
-        d="${i}"
-      fi
-      ip="192.168.2.2${d}"
-      directory="S${d}"
-      
-      echo "Hole den Inhalt des Verzeichnisses ~/Austausch von Gerät S${d} (IP: ${ip}) …"
-      
-      percentage=$(bc <<< "${i} * 100 / 15")
-      #echo "$percentage"; echo "# Hole Daten von Rechner S${d} …" | zenity --progress --text "# Hole Daten von Rechner S${d} …" --percentage=0
-      
-      scp -r -P 32400 schueler@${ip}:/home/schueler/Schreibtisch/Austausch/* "/home/lehrer/Schreibtisch/Eingesammelt/${directory}"
-      
-      i=$(( $i + 1 ))
+if [ $ANSWER = "0" ]; then
+
+    JOBS_TOTAL=$(($LAST_IP - $FIRST_IP + 1))
+    STARTED=0
+    JOBS=()
+    for i in $(seq $FIRST_IP $LAST_IP); do
+      fetch_from $i &
+      JOBS[$STARTED]="$!"
+      STARTED=$((STARTED + 1))
     done
 
-    #zenity --info --text="Alles eingesammelt." --width=250
-    notify-send -i /usr/share/icons/down -t 20000 "Einsammeln abgeschlossen" "Das Einsammeln wurde abgeschlossen."
-    sleep 20
+    RUNNING=$JOBS_TOTAL
+    (
+      while [ $RUNNING -gt 0 ]; do
+        RUNNING=0
+        for PID in ${JOBS[@]}; do
+            if kill -0 "$PID" > /dev/null 2>&1; then
+              RUNNING=$(($RUNNING + 1))
+            fi
+        done
+
+        JOBS_FINISHED=$(($JOBS_TOTAL - $RUNNING))
+        PERCENTAGE=$(bc <<< "$JOBS_FINISHED * 100 / $JOBS_TOTAL")
+        echo "# ${JOBS_FINISHED}/${JOBS_TOTAL} abgeschlossen (${PERCENTAGE}%)."
+        echo $PERCENTAGE
+
+        sleep 1
+        echo "STILL RUNNING: ${RUNNING}"
+      done
+    ) | zenity --progress --text "Warte auf Daten …" --percentage=0 --auto-close --width=500
+
+    if [ "$?" = -1 ] ; then
+      echo "Einsammeln abgebrochen."
+      notify-send -i /usr/share/icons/down "Einsammeln abgebrochen."
+    else
+      notify-send -i /usr/share/icons/down -t 20000 "Einsammeln abgeschlossen" "Das Einsammeln wurde abgeschlossen."
+    fi
 
 else
     echo "Abbruch."
-    #(
-    #echo "25"; sleep 1
-    #echo "# Statusmeldung 1"
-    #echo "50"; sleep 1
-    #echo "# Statusmeldung 2"
-    #echo "100"
-    #) | zenity --progress --text "Vorgang wird bearbeitet" --percentage=0
     notify-send -i /usr/share/icons/down "Einsammeln abgebrochen."
 fi
