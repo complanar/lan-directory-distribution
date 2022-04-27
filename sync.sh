99daedefec8e9d989beaab2b56550b107cf0a763
cgloeckner <cgloeckner@freenet.de>
2022-04-27 18:52:54 +0200

    added ui confirm/notification

--- sync.sh
+++ sync.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# ---------------------------------------------------------------------
+# --- Paths and Constants ---------------------------------------------
+# ---------------------------------------------------------------------
+
 FETCH_PATH="/home/lehrer/Schreibtisch/Eingesammelt"
 SHARE_PATH="/home/lehrer/Schreibtisch/Austeilen"
 SHARE_ALL_PATH="$SHARE_PATH/Alle"
@@ -9,6 +13,10 @@ DEVICES_FROM=1
 DEVICES_TO=15
 
 PORT=32400
+
+# ---------------------------------------------------------------------
+# --- Utility Functions -----------------------------------------------
+# ---------------------------------------------------------------------
 
 # Return $1 as string with two digits
 get_two_digits () {
@@ -33,14 +41,16 @@ get_ip () {
 get_ssh_login () {
     echo "schueler@$( get_ip $1 )"
 }
-
-# ---------------------------------------------------------------------
-
+ 
 # Copy files via SSH from $1 to $2
 copy_via_ssh () {
     # TODO: remove echo after testing
     echo "scp -r -P ${PORT} ${1}/* ${2}/*"
 }
+
+# ---------------------------------------------------------------------
+# --- Actual sharing/fetching -----------------------------------------
+# ---------------------------------------------------------------------
 
 # Share with devices (optional: from $1, else individual directory)
 share () {
@@ -57,13 +67,13 @@ share () {
         fi
         
         DST="${LOGIN}:${EXCHANGE_PATH}"
-        copy_via_ssh $SRC $DST 
-
+        copy_via_ssh $SRC $DST
+        
         if [ -z "$1" ]; then
             # clear individual share directory+
             # TODO: remove echo after testing
             echo "rm -r ${SRC}"
-        fi
+        fi 
     done
 }
 
@@ -79,15 +89,68 @@ fetch () {
 }
 
 # ---------------------------------------------------------------------
+# --- UI Utilities ----------------------------------------------------
+# ---------------------------------------------------------------------
+
+confirm_share_each () {
+    QUESTION="Die Dateien in in\n\n${SHARE_PATH}/$( get_dir_name $DEVICES_FROM )\nbis\n${SHARE_PATH}/$( get_dir_name $DEVICES_TO )\n\nwerden ausgeteilt. Dies kann einen Moment dauern. Sie werden benachrichtigt, wenn das Austeilen abgeschlossen ist.\n\nWARNUNG: Die genannten Ausgabe-Ordner werden anschließend VOLLSTÄNDIG GELEERT. Stellen Sie sicher, dass Sie eine KOPIE der Daten besitzen.\n\nFortfahren?"
+    zenity --question --text="$QUESTION" --width=500
+    echo "$?"
+}
+
+confirm_share_all () {
+    QUESTION="Die Dateien in\n\n${SHARE_ALL_PATH}\n\nwerden ausgeteilt. Dies kann einen Moment dauern. Sie werden benachrichtigt, wenn das Austeilen abgeschlossen ist."
+    zenity --question --text="$QUESTION" --width=500
+    echo "$?"
+}
+
+confirm_fetch () {
+    QUESTION="Die Dateien der Schüler werden eingesammelt und in\n\n${FETCH_PATH}/$( get_dir_name $DEVICES_FROM )\nbis\n${FETCH_PATH}/$( get_dir_name $DEVICES_TO )\n\n abgespeichert. Dies kann einen Moment dauern. Sie werden benachrichtigt, wenn das Austeilen abgeschlossen ist."
+    zenity --question --text="$QUESTION" --width=500
+    echo "$?"
+}
+
+notify_success() {
+    notify-send -i /usr/share/icons/up -t 20000 "Abgeschlossen" "Das $1 wurde abgeschlossen."
+}
+
+notify_abort () {
+    notify-send -i /usr/share/icons/up "Abgebrochen" "Der Benutzer hat die Aktion abgebrochen"
+}
+
+# ---------------------------------------------------------------------
+# --- Main Program ----------------------------------------------------
+# ---------------------------------------------------------------------
 
 if [ "$1" == "--share-each" ]; then
-    share
+    ANSWER=$( confirm_share_each )
 
-elif [ "$1" == "--share-all" ]; then
-    share $SHARE_ALL_PATH
+    if [ $ANSWER = "0" ]; then 
+        share
+        notify_success "individuelle Austeilen"
+    else
+        notify_abort
+    fi
+
+elif [ "$1" == "--share-all" ]; then 
+    ANSWER=$( confirm_share_all )
+
+    if [ $ANSWER = "0" ]; then 
+        share $SHARE_ALL_PATH
+        notify_success "einheitliche Austeilen"
+    else
+        notify_abort
+    fi
     
-elif [ "$1" == "--fetch" ]; then
-    fetch
+elif [ "$1" == "--fetch" ]; then 
+    ANSWER=$( confirm_fetch )
+
+    if [ $ANSWER = "0" ]; then
+        fetch
+        notify_success "Einsammeln"
+    else
+        notify_abort
+    fi
 
 else
     cat USAGE.md
