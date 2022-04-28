8c0e5676afd1ed1877c9af3292f1ade16994dcc7
Holger Teichert <post@complanar.de>
2022-04-28 12:40:14 +0200

    Add multithreading and progress bar

--- sync.sh
+++ sync.sh
@@ -4,10 +4,18 @@
 # --- Paths and Constants ---------------------------------------------
 # ---------------------------------------------------------------------
 
-FETCH_PATH="/home/lehrer/Schreibtisch/Eingesammelt"
-SHARE_PATH="/home/lehrer/Schreibtisch/Austeilen"
+IP_TEMPLATE="192.168.2.2xx"
+
+IP_PREFIX=${IP_TEMPLATE%%x*}
+PREFLEN=$(( ${#IP_TEMPLATE} - ${#IP_PREFIX} ))
+
+REMOTE_USER="schueler"
+EXCHANGE_PATH="/home/${REMOTE_USER}/Schreibtisch/Austausch"
+
+LOCAL_USER="lehrer"
+FETCH_DIR="/home/${LOCAL_USER}/Schreibtisch/Eingesammelt"
+SHARE_PATH="/home/${LOCAL_USER}/Schreibtisch/Austeilen"
 SHARE_ALL_PATH="$SHARE_PATH/Alle"
-EXCHANGE_PATH="/home/schueler/Schreibtisch/Austausch"
 
 DEVICES_FROM=1
 DEVICES_TO=15
@@ -18,34 +26,35 @@ PORT=32400
 # --- Utility Functions -----------------------------------------------
 # ---------------------------------------------------------------------
 
-# Return $1 as string with two digits
-get_two_digits () {
-    if [ $1 -le 9 ]; then
-        echo "0$1"
-    else
-        echo "$1"
-    fi 
+# Return $1 as string with $2 digits
+get_n_digits() {
+    NUM=$1
+    while [ ${#NUM} -lt $2 ]; do
+        NUM="0${NUM}"
+    done
+    echo $NUM
 }
 
 # Return local directory name for device $1
 get_dir_name () {
-    echo "S$( get_two_digits $1)"
+    echo "S$( get_n_digits $1 $PREFLEN)"
 }
 
 # Return IP for device $1
-get_ip () {
-    echo "192.168.2.2$( get_two_digits $1)"
+get_ip() {
+  echo "${IP_TEMPLATE%%x*}$(get_n_digits $1 $PREFLEN)"
 }
 
-# Return SSH login
+# Return SSH login for device $1
 get_ssh_login () {
-    echo "schueler@$( get_ip $1 )"
+    echo "${REMOTE_USER}@$( get_ip $1 )"
 }
- 
+
 # Copy files via SSH from $1 to $2
 copy_via_ssh () {
     # TODO: remove echo after testing
-    echo "scp -r -P ${PORT} ${1}/* ${2}/*"
+    $(echo "scp -r -P ${PORT} ${1}/* ${2}/*")
+    sleep 2
 }
 
 # ---------------------------------------------------------------------
@@ -56,43 +65,54 @@ copy_via_ssh () {
 share () {
     # setup default directory (e.g. $SHARE_ALL_PATH)
     SRC=$1
-    
+
+    i=0
+    JOBS=()
     for ((k=DEVICES_FROM;k<=DEVICES_TO;k++)); do
         LOGIN=$( get_ssh_login $k )
         DIR=$( get_dir_name $k )
-        
+
         if [ -z "$1" ]; then
             # share from individual directory
             SRC="${SHARE_PATH}/${DIR}"
         fi
-        
+
         DST="${LOGIN}:${EXCHANGE_PATH}"
-        copy_via_ssh $SRC $DST
-        
+        copy_via_ssh $SRC $DST &
+        # Catch process ids
+        JOBS[$i]=$!
+        i=$((i + 1))
+
         if [ -z "$1" ]; then
             # clear individual share directory+
             # TODO: remove echo after testing
             echo "rm -r ${SRC}"
-        fi 
+        fi
     done
 }
 
 # Fetch from all devices
 fetch () {
+    i=0
+    JOBS=()
     for ((k=DEVICES_FROM;k<=DEVICES_TO;k++)); do
         LOGIN=$( get_ssh_login $k )
         DIR=$( get_dir_name $k )
         SRC="${LOGIN}:${EXCHANGE_PATH}"
-        DST="${SHARE_PATH}/${DIR}"        
-        copy_via_ssh $SRC $DST
+        DST="${SHARE_PATH}/${DIR}"
+
+        copy_via_ssh $SRC $DST &
+        JOBS[$i]=$!
+        i=$((i + 1))
     done
+    #echo ${JOBS[@]}
 }
 
 # Create ZIP-file from fetched data
 create_zip () {
     # create filename suggestion
     FILENAME=`date +"%Y-%m-%d_%H-%m-%S"`
-    
+
     # query actual filename
     FILENAME=$( query_zip_name ${FILENAME} )
     if [[ "$FILENAME" != *.zip ]]; then
@@ -133,15 +153,36 @@ ask_to_zip () {
 }
 
 notify_success() {
-    notify-send -i /usr/share/icons/up -t 20000 "Abgeschlossen" "Das $1 wurde abgeschlossen."
+    notify-send -i /usr/share/icons/$1 -t 20000 "Abgeschlossen" "Das $2 wurde abgeschlossen."
 }
 
 notify_abort () {
-    notify-send -i /usr/share/icons/down "Abgebrochen" "Der Benutzer hat die Aktion abgebrochen"
+    notify-send -i /usr/share/icons/$1 "Abgebrochen" "Der Benutzer hat die Aktion abgebrochen"
 }
 
 query_zip_name () {
     zenity --file-selection --title="ZIP-Datei erstellen" --filename=$1 --save --confirm-overwrite --file-filter=*.zip
+}
+
+progess_bar () {
+  RUNNING=${#JOBS[@]}
+  (
+    while [ $RUNNING -gt 0 ]; do
+      RUNNING=0
+      for PID in ${JOBS[@]}; do
+          if kill -0 "$PID" > /dev/null 2>&1; then
+            RUNNING=$(($RUNNING + 1))
+          fi
+      done
+
+      JOBS_FINISHED=$((${#JOBS[@]} - $RUNNING))
+      PERCENTAGE=$(bc <<< "$JOBS_FINISHED * 100 / ${#JOBS[@]}")
+      echo "# ${JOBS_FINISHED}/${#JOBS[@]} abgeschlossen (${PERCENTAGE}%)."
+      echo $PERCENTAGE
+
+      sleep 0.5
+    done
+  ) | zenity --progress --text "Übertrage auf Daten …" --percentage=0 --auto-close --width=500
 }
 
 # ---------------------------------------------------------------------
@@ -151,39 +192,46 @@ query_zip_name () {
 if [ "$1" == "--share-each" ]; then
     ANSWER=$( confirm_share_each )
 
-    if [ $ANSWER = "0" ]; then 
+    if [ $ANSWER = "0" ]; then
         share
-        notify_success "individuelle Austeilen"
+        progess_bar
+        notify_success "up" "individuelle Austeilen"
     else
-        notify_abort
+        notify_abort "up"
     fi
 
-elif [ "$1" == "--share-all" ]; then 
+elif [ "$1" == "--share-all" ]; then
     ANSWER=$( confirm_share_all )
 
-    if [ $ANSWER = "0" ]; then 
+    if [ $ANSWER = "0" ]; then
         share $SHARE_ALL_PATH
-        notify_success "einheitliche Austeilen"
+        progess_bar
+        notify_success "up" "einheitliche Austeilen"
     else
-        notify_abort
+        notify_abort "up"
     fi
-    
-elif [ "$1" == "--fetch" ]; then 
+
+elif [ "$1" == "--fetch" ]; then
     ANSWER=$( confirm_fetch )
 
     if [ $ANSWER = "0" ]; then
+        # UGLY: Working with global variables
+        # Didn't find a way to return the PIDs from a subprocess without
+        # breaking the multithreading
         fetch
-        notify_success "Einsammeln"
+        progess_bar
+
+        notify_success "down" "Einsammeln"
 
         # ask to zip
         ANSWER=$( ask_to_zip )
-        
+
         if [ $ANSWER = "0" ]; then
             create_zip
         fi
 
     else
-        notify_abort
+        notify_abort "down"
     fi
 
 else
