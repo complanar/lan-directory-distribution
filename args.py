c19c68beb21ae06a0bed926f0900ae2bcd086dde
cgloeckner <cgloeckner@freenet.de>
2022-04-29 19:23:29 +0200

    added **kwargs for cli arg callback

--- args.py
+++ args.py
@@ -8,10 +8,10 @@ class CliArgs(object):
     def register(self, option, callback):
         self.lookup[option] = callback
 
-    def __call__(self, argv):
+    def __call__(self, argv, **kwargs):
         for key in argv:
             if key in self.lookup:
-                self.lookup[key]()
+                self.lookup[key](**kwargs)
                 return True
         return False
 
