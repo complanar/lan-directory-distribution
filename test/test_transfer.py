90c31cb654f0b554f6c3b150f098494e94a6b18e
cgloeckner <cgloeckner@freenet.de>
2022-04-29 18:32:04 +0200

    scp-based transfer worker with unittest [unfinished]

---
+++ test/test_transfer.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+import unittest, ipaddress
+
+from settings import Settings
+from transfer import TransferWorker, batch
+
+class DummyProgress(object):
+    def __init__(self):
+        self.value = 0.0
+    
+    def __call__(self, value):
+        self.value = value
+
+    def getStatus(self):
+        if self.value == 1.0:
+            return True
+        return None
+
+# ---------------------------------------------------------------------
+
+class DiscoveryTest(unittest.TestCase):
+        
+    def setUp(self):
+        self.settings = Settings()
+        self.settings.first_ip    = ipaddress.ip_address('0.0.0.1')
+        self.settings.num_clients = 5 
+        self.settings.user        = 'tester'
+
+        self.settings.folder_prefix = 'PC'
+        self.settings.exchange      = '~/exchange'
+        self.settings.fetch         = '~/fetch'
+        self.settings.share         = '~/share'
+        self.settings.shareall      = '~/shareAll'
+        
+    def tearDown(self):
+        del self.settings
+
+    def test_TransferWorker(self):
+        w = TransferWorker('/tmp/from', 'tester@0.0.0.1:/tmp/to')
+        w.join()
+
+    def test_batch(self):
+        p = DummyProgress()
+        devices = [1, 3, 4]
+        src_lambda = self.settings.getExchangeDir
+        dst_lambda = self.settings.getFetchDir
+        status = batch(devices, src_lambda, dst_lambda, p)
+        self.assertTrue(status) 
---
+++ transfer.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import subprocess, threading, time
+
+
+class TransferWorker(threading.Thread):
+    """Thread to handle copy via SSH."""
+    
+    def __init__(self, src, dst, port=32400, timeout=3):
+        """Transer files from {src} to {dst}."""
+        super().__init__()
+        self.src     = src
+        self.dst     = dst
+        self.port    = port
+        self.timeout = timeout
+        self.start()
+
+    def run(self):
+        cmd = f'scp -o ConnectTimeout={self.timeout} -rP {self.port} {self.src} {self.dst}'
+        subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
+
+
+def batch(devices, src_lambda, dst_lambda, progress, delay=0.1):
+    """Batch transfer for {devices}. Source and destination folders will
+be picked based on the actual device using {src_lambda} and {dst_lambda}.
+"""
+    # setup workers
+    worker = list()
+    for device in devices:
+        src = src_lambda(device)
+        dst = dst_lambda(device)
+        worker.append(TransferWorker(src, dst))
+    
+    # wait and update progress bar
+    num_devices = len(devices)
+    n = num_devices
+    while n > 0 and progress.getStatus() is None:
+        n = threading.activeCount() - 1
+        progress((num_devices - n) / num_devices)
+        time.sleep(delay)
+    
+    # FIXME: progress.getStatus() isn't working correctly; failed SCPs are not checked
+    return n == 0
